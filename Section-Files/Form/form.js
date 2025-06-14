a39ea64a0a9b9fd0e6312e12385a7a5a13934068
Jaisal <jkmehta@iu.edu>
2025-06-14 00:51:42 -0500

    website edit commit

--- Section-Files/Form/form.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwXfzGBuuUnXZFRNXB4-i0VgDqT1eELtQeomNjI4jRWM8tMtsvzQPiXAC5SyKeEqUHDiw/exec'
-const form = document.forms['contact-form']
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Form submitted"))
-    .then(() => { window.location.reload(); })
-    .catch(error => console.error('Error!', error.message))
-});
